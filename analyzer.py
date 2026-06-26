d6022fed55025c87d25e3177d8cfde5e5b734ac3
vijay sai <251331155+vjsai5@users.noreply.github.com>
2026-06-26 18:10:33 +0530

    Delete analyzer.py

--- analyzer.py
+++
@@ -1,6 +0,0 @@
-
-from utils.llm import LLMClient
-class AnalyzerAgent:
-    def __init__(self): self.llm=LLMClient()
-    async def run(self,data):
-        return self.llm.generate("Analyze and summarize:\n"+str(data))
